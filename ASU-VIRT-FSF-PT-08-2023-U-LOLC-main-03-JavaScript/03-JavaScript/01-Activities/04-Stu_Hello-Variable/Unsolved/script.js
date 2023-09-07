4d8b9fcf1151530361455dcfbfc712d8b1523881
breashroyer <brdenino@gmail.com>
2023-09-07 13:43:39 -0700

    deleted unwanted ASU file that included all of the activities for Week 3

--- ASU-VIRT-FSF-PT-08-2023-U-LOLC-main-03-JavaScript/03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++
@@ -1,8 +0,0 @@
-// Get a reference to the output element
-const outputElement = document.getElementById('output');
-
-// Log a message to the console
-console.log('Hello, world!');
-
-// Update the content of the output element with the logged message
-outputElement.textContent = 'Hello, world!';
