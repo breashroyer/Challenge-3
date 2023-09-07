c0aa24ae3801d8c0e83215d1f912ab0d11016073
breashroyer <brdenino@gmail.com>
2023-09-07 13:21:45 -0700

    Initial commit

---
+++ ASU-VIRT-FSF-PT-08-2023-U-LOLC-main-03-JavaScript/03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,8 @@
+// Get a reference to the output element
+const outputElement = document.getElementById('output');
+
+// Log a message to the console
+console.log('Hello, world!');
+
+// Update the content of the output element with the logged message
+outputElement.textContent = 'Hello, world!';
